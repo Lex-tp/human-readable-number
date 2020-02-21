46992aafc5d1e02737c264908abd95060c3d8d49
Denis Korotevich <volk.denis98@gmail.com>
2020-02-22 00:52:42 +0300

    feat: solve the problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    if (number != '') {
+        let str = '';
+        str += toWords((parseInt((number / 100) % 10)), "hundred ");
+        str += toWords(parseInt((number % 100)), "");
+        return str.substr(0, str.length - 1);
+    }
+    return 'zero';
 }
+
+function toWords(number, s) {
+    let str = '';
+    if (number > 19)
+        str += secondDig[parseInt(number / 10)] + firstDig[number % 10];
+    else
+        str += firstDig[number];
+    if (number)
+        str += s;
+
+    return str;
+}
+
+let firstDig = ['', 'one ', 'two ', 'three ', 'four ',
+    'five ', 'six ', "seven ", 'eight ',
+    'nine ', 'ten ', 'eleven ', 'twelve ',
+    'thirteen ', 'fourteen ', 'fifteen ',
+    'sixteen ', 'seventeen ', 'eighteen ',
+    'nineteen '];
+
+let secondDig = ['', '', 'twenty ', 'thirty ', 'forty ',
+    'fifty ', 'sixty ', 'seventy ', 'eighty ',
+    'ninety '];
